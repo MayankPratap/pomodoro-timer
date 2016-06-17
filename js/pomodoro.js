500bd06282e950830ec51056d4f3b7d48afce1e5
Mayank Pratap <mayank7860@gmail.com>
2016-06-17 21:48:38 +0530

    Button click to change session length implemented

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,39 @@
 $(document).ready(function(){
 
+   $('#break-length').val(5);
+   $('#session-length').val(25);
+
    $('#break-minus').click(function(){
       console.log($('#break-length').val());
-      $('#break-length').val($('#break-length').val-1);
+      var newTime=parseInt($('#break-length').val())-1;
+      if(newTime<0)
+        newTime=0;
 
+      $('#break-length').val(newTime.toString());
 
    });
 
+   $('#break-plus').click(function(){
 
+      var newTime=parseInt($('#break-length').val())+1;
+      $('#break-length').val(newTime.toString());
 
+   });
 
+   $('#session-minus').click(function(){
 
+      var newTime=parseInt($('#session-length').val())-1;
+      if(newTime<0)
+        newTime=0;
+      $('#session-length').val(newTime.toString());
+
+   });
+
+   $('#session-plus').click(function(){
+
+      var newTime=parseInt($('#session-length').val())+1;
+      $('#session-length').val(newTime.toString());
+
+   });
+   
 });
