7bf34ab82e84fab59ff71836023de20301c04088
Mayank Pratap <mayank7860@gmail.com>
2016-06-17 21:37:37 +0530

    Adding Bootstrap and JQuery JS files

---
+++ js/pomodoro.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+   $('#break-minus').click(function(){
+      console.log($('#break-length').val());
+      $('#break-length').val($('#break-length').val-1);
+
+
+   });
+
+
+
+
+
+});
