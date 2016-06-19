c050ac3731ccea956d35a8a21d73e6644a97428d
Mayank Pratap <mayank7860@gmail.com>
2016-06-19 17:13:33 +0530

    Now Break Length,Session Length and Timer Working

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -3,16 +3,21 @@ $(document).ready(function(){
    var status=0; // Represents that timer is off now. Click on circle/ellipse to turn on//
    $('#break-length').val("5");
    $('#session-length').val("130");
-   updateTime();
+   var time;
+   time=parseInt($('#session-length').val());
+   time=time*60;
+   updateTime(time);
+   /**** Function to update time on timer *****/
 
-   function updateTime(){
+   function updateTime(nTime){
     // function to update time
-     time=parseInt($('#session-length').val());
-     time=time*60;
      var showTime="";
-     var h=Math.floor(time/3600);
-     var m=Math.floor(time/60-h*60);
-     var s=Math.floor(time%60);
+     var h=Math.floor(nTime/3600);
+     var m=Math.floor(nTime/60-h*60);
+     var s=Math.floor(nTime%60);
+     //console.log(h);
+     //console.log(m);
+     //console.log(s);
      if(h!=0){
          if(h<10)
            showTime+="0"+h.toString();
@@ -41,120 +46,78 @@ $(document).ready(function(){
        showTime+=":00";
 
       $('#time').text(showTime);
-
    }
 
-    $('#break-minus').click(function(){
-       console.log($('#break-length').val());
-       var newTime=parseInt($('#break-length').val())-1;
-       if(newTime<0)  // No body can reduce time beyond zero
-          newTime=0;
+   /***** Button Click Events *****/
 
-       $('#break-length').val(newTime.toString());
+    $('#break-minus').click(function(){
+       if(status===0){  // Change break length only when timer is paused.
+         console.log($('#break-length').val());
+         var newTime=parseInt($('#break-length').val())-1;
+         if(newTime<0)  // No body can reduce time beyond zero
+           newTime=0;
+         $('#break-length').val(newTime.toString());
+      }
     });
 
     $('#break-plus').click(function(){
-
-       var newTime=parseInt($('#break-length').val())+1;
-       if(newTime>1440) // You cannot increase time more than one day.
-         newTime=1440;
-       $('#break-length').val(newTime.toString());
+       if(status===0){
+          var newTime=parseInt($('#break-length').val())+1;
+          if(newTime>1440) // You cannot increase time more than one day.
+            newTime=1440;
+          $('#break-length').val(newTime.toString());
+       }
     });
 
     $('#session-minus').click(function(){
-
-       var newTime=parseInt($('#session-length').val())-1;
-       if(newTime<0)
-         newTime=0;
-       $('#session-length').val(newTime.toString());
-       updateTime();  // update session time in timer
-
+       if(status===0){
+         var newTime=parseInt($('#session-length').val())-1;
+         if(newTime<0)
+            newTime=0;
+         $('#session-length').val(newTime.toString());
+         newTime*=60;  // Convert newTime to seconds before sending to updateTime function
+         console.log(newTime);
+         updateTime(newTime);
+      }
    });
 
    $('#session-plus').click(function(){
-
-      var newTime=parseInt($('#session-length').val())+1;
-      if(newTime>1440)  // Not more than 24 hours :P
-        newTime=1440;
-      $('#session-length').val(newTime.toString());
-      updateTime();  // Update session time in timer.
+      if(status===0){
+         var newTime=parseInt($('#session-length').val())+1;
+         if(newTime>1440)  // Not more than 24 hours :P
+           newTime=1440;
+         $('#session-length').val(newTime.toString());
+         newTime*=60;  // Convert newTime to seconds before sending to updateTime function
+         console.log(newTime);
+         updateTime(newTime);  // Update session time in timer.
+      }
    });
 
    $('#data').click(function(){
 
-      if(status===0)
-        status=1;   // Timer is resumed ...
-      else if(status==1)
-         status=0;  // Timer is paused .
+       if(status===0)
+         status=1;
+       else if(status==1)
+         status=0;
+       if(status==1){
 
-      if(status==1){
-
-         var time=parseInt($('#session-length').val());
+         time=parseInt($('#session-length').val());
          time=time*60;
-         console.log(time);
-
          function sessionStart(){
-            time-=1;
-            if(time===0){
-                tryTakingBreak();
-            }
-
-            $('#type').text("Session");
-
-            var showTime="";
-
-            var h=Math.floor(time/3600);
-            var m=Math.floor(time/60);
-            var s=Math.floor(time%60);
-
-            if(h!=0){
-                if(h<10)
-                  showTime+="0"+h.toString();
-                else
-                   showTime+=h.toString();
-            }
-            else
-              showTime+="00";
-            if(m!=0){
-
-                showTime+=":";
-                if(m<10)
-                   showTime+="0"+m.toString();
-                else
-                   showTime+=m.toString();
-            }
-            else{
-
-               showTime+=":00";
-            }
-            if(s!=0){
-
-                showTime+=":";
-                if(s<10)
-                   showTime+="0"+s.toString();
-                else
-                   showTime+=s.toString();
-            }
-            else{
-             showTime+=":00";
-            }
-            console.log(showTime);
-            console.log($('#time').text());
-            $('#time').text(showTime);
-
+            time=time-1;
             console.log(time);
+            if(time===0)
+              tryTakingBreak();
+            $('#type').text("Session");
+            updateTime(time);
          }
          var t=setInterval(sessionStart,1000);
-
          function tryTakingBreak(){
-           console.log("Break Time");
-           clearInterval(t);
+            console.log("Break Time");
+            clearInterval(t);
          }
 
-      }
-
-
-
+       }
 
    });
 
